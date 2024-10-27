26739d5b089235029d5afb0a88cad0cd59d6058a
Moheed-Dar <moheeddar324@gmail.com>
2024-10-27 19:48:43 +0500

    this is new project

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+// import 'bootstrap/dist/css/bootstrap.min.css';
+// // import { Button } from "react-bootstrap"; 
+import {BrowserRouter,Routes,Route} from 'react-router-dom'
+import House from './Component/home'
+import Detail from './Component/detail'
+function App() {
+  return (
+    <div className="App">
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={ <House/>}/>
+            <Route path="/Detail" element={ <Detail/>}/>
+          </Routes>
+        </BrowserRouter>
+    </div>
+  );
+}
+export default App;
---
+++ src/Component/home.jsx
@@ -0,0 +1,8 @@
+function House(){
+    return(
+        <div>
+            <h1>house page.</h1>
+        </div>
+    )
+}
+export default House
