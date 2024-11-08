9756d40ea4872a331378a4590dcdb1b08776900f
Moheed-Dar <moheeddar324@gmail.com>
2024-11-08 20:21:11 +0500

    context api 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,39 @@
 // import 'bootstrap/dist/css/bootstrap.min.css';
 // // import { Button } from "react-bootstrap"; 
-import React,{} from 'react';
+import React, { useEffect,useState } from 'react';
 import './App.css'
-import UserContextProvider from './context/UserContextProvider';
-import Login from './components/Login';
-import Profile from './components/Profile';
-
-
+import { ThemeProvider } from './contexts/theme';
+import ThemeBtn from './compoenets/ThemeBtn';
+import Card from './compoenets/Card';
 
 function App() {
+  const [themeMode,setThemeMode] =useState("light")
+  const lightTheme = ()=>{
+    setThemeMode("light")
+  }
+  const darkTheme = ()=>{
+    setThemeMode("dark")
+  }
+  useEffect(()=>{
+    document.querySelector('html').classList.remove("light","dark")
+    document.querySelector('html').classList.add(themeMode)
+  },[themeMode])
+
   return (
-    <div className="App">
-      <UserContextProvider>
-      <h1>React</h1>
-      <Login />
-      <Profile />
-      </UserContextProvider>
-    </div>
+    <ThemeProvider  value={{themeMode,darkTheme,lightTheme}} >
+      <div className="flex flex-wrap min-h-screen items-center">
+        <div className="w-full">
+          <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
+            {/* {theme button} */}
+            <ThemeBtn />
+          </div>
+          <div className="w-full max-w-sm mx-auto">
+          {/* {card} */}
+          <Card />
+          </div>
+        </div>
+      </div>
+    </ThemeProvider>
   );
 }
 export default App;
